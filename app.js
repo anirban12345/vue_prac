e7f7f491b970a52d5e8ae0bf4df666998559821c
anirban12345 <you@example.com>
2022-02-23 14:25:10 +0530

    .

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
     data(){
         return {
             firstName:'Anirban',
+            lastName:'Seth',
+            emailId:'anirban54321@gmail.com',
+            phone:'8877445566',
+            image:'userAnirban.jpg'
         }
     },
 })
