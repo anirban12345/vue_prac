5f92496c40238ff83203f174ce6d0d8ef47aece7
anirban12345 <you@example.com>
2022-02-23 14:59:22 +0530

    .

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ const app = Vue.createApp({
             firstName:'Anirban',
             lastName:'Seth',
             emailId:'anirban54321@gmail.com',
+            gender:'male',
             phone:'8877445566',
-            image:'userAnirban.jpg'
+            image:'https://randomuser.me/api/portraits/men/11.jpg'
+        }
+    },
+    methods:{
+        async getUser(){
+            //console.log(this.firstName);
+
+            const res=await fetch('https://www.randomuser.me/api');
+            const data= await res.json();
+
+            //console.log(data.results[0]);
+
+            //console.log(data.results[0].name.first);
+
+            this.firstName=data.results[0].name.first
+            this.lastName=data.results[0].name.last
+            this.emailId=data.results[0].email
+            this.gender=data.results[0].gender
+            this.phone=data.results[0].phone
+            this.image=data.results[0].picture.large
         }
     },
 })
